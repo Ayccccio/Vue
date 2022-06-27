c83a61cf89183e936d59b7c694829dac318c233d
Ayccccio_home <Ayccccio@gmail.com>
2022-06-27 21:21:35 +0800

    ref属性

---
+++ 01.脚手架环境/my-project/02. src_ref属性/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1 ref="app">这里是APP</h1>
+        <Student ref="vc"></Student>    
+        <button ref="btn" @click="showRefs">点击打印refs</button>
+    </div>
+</template>
+
+<script>
+    import Student from './components/Student.vue'
+export default {
+    name: 'App',
+    components: {
+        Student
+    },
+    methods: {
+        showRefs(){
+            console.log(this.$refs)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
