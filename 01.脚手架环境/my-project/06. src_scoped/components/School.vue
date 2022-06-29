52157b39a6a0186ec1292928a56d270f927b315f
Ayccccio_home <Ayccccio@gmail.com>
2022-06-29 20:32:41 +0800

    scoped

---
+++ 01.脚手架环境/my-project/06. src_scoped/components/School.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>School</h1>
+        <h1>School</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'School'
+}
+</script>
+
+<style scoped>
+    h1{
+        background-color: skyblue;
+    }
+</style>
---
+++ 01.脚手架环境/my-project/06. src_scoped/components/Student.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Student</h1>
+        <h1>Student</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Student'
+}
+</script>
+
+
+<style scoped>
+    h1{
+        border: 1px solid #000;
+    }
+</style>
