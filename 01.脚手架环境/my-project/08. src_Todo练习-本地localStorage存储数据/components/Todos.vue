50a814eb32121f9b58a27642a4d06978eb6decbc
Ay_HY <Ayccccio@outlook.com>
2022-07-02 17:56:56 +0800

    [todoList]利用localStorage本地存储数据,实现用户刷新,退出保留数据.

---
+++ 01.脚手架环境/my-project/08. src_Todo练习-本地localStorage存储数据/components/Todos.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="todo-main">
+        <Todo v-for="todo in todoList" :key="todo.id" :todo="todo" :handleTodo='handleTodo'></Todo>
+    </ul>
+</template>
+
+<script>
+    import Todo from './Todo'
+export default {
+    components: {
+        Todo
+    },
+    props: ['todoList','handleTodo']
+}
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+}
+
+.todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+}
+</style>
