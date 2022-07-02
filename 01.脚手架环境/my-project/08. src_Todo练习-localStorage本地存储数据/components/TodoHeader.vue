eac9e76b7fffe3674040effc1777785ae5fcb356
Ayccccio_home <Ayccccio@gmail.com>
2022-07-02 19:56:15 +0800

    [todoList]利用localStorage本地存储数据,实现刷新,关闭窗口不丢失数据.

---
+++ 01.脚手架环境/my-project/08. src_Todo练习-localStorage本地存储数据/components/TodoHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="todo-header">
+        <input type="text" placeholder="请输入你的任务名称，按回车键确认" @keyup.enter="addTodo"/>
+    </div>
+</template>
+
+<script>
+    import {nanoid} from 'nanoid'
+export default {
+    name: 'TodoHeader',
+    props: ['handleTodo'],
+    methods:{
+        addTodo(e){
+            this.handleTodo[2]({id:nanoid(), name:e.target.value, done:false})
+            e.target.value = ''
+        }
+    }
+};
+</script>
+
+<style scoped>
+/*header*/
+.todo-header input {
+    width: 560px;
+    height: 28px;
+    font-size: 14px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 4px 7px;
+}
+</style>
