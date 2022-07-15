4131383c8f34421a4304638d42cfb7313f09bca6
Ayccccio_home <Ayccccio@gmail.com>
2022-07-15 22:31:08 +0800

    具名插槽

---
+++ 01.脚手架环境/my-project/16. src_具名插槽/App.vue
@@ -0,0 +1,72 @@
+<template>
+    <div id="app">
+        <Category type="美食">
+            <img slot="content" src="https://s3.ax1x.com/2021/01/16/srJlq0.jpg" alt />
+            <div slot="footer">
+                <a href="#">美食传送门</a>
+                <a href="#">更多美食</a>
+            </div>
+        </Category>
+        <Category type="游戏">
+            <ul slot="content">
+                <li v-for="(food,index) in foods">{{food}}</li>
+            </ul>
+            <div slot="footer">
+                <a href="#">游戏传送门</a>
+                <a href="#">更多游戏</a>
+            </div>
+        </Category>
+        <Category type="电影">
+            <video slot="content" controls src ="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"></video>
+            <div slot="footer">
+                <a href="#">游戏传送门</a>
+                <a href="#">更多游戏</a>
+            </div>
+        </Category>
+    </div>
+</template>
+
+<script>
+import Category from "./components/Category.vue";
+export default {
+    name: "App",
+    components: { Category },
+    data() {
+        return {
+            foods: ["火锅", "烧烤", "小龙虾", "牛排"],
+            games: ["红色警戒", "穿越火线", "劲舞团", "超级玛丽"],
+            films: [
+                "《教父》",
+                "《拆弹专家》",
+                "《你好，李焕英》",
+                "《尚硅谷》",
+            ],
+        };
+    },
+};
+</script>
+
+<style lang='less'>
+#app {
+    display: flex;
+    justify-content: space-evenly;
+    .category {
+        width: 200px;
+        height: 300px;
+        background-color: #85a4ed;
+        h3 {
+            background-color: orange;
+            text-align: center;
+            font-weight: bold;
+        }
+        img,
+        video {
+            width: 100%;
+        }
+        div {
+            display: flex;
+            justify-content: space-evenly;
+        }
+    }
+}
+</style>
---
+++ 01.脚手架环境/my-project/16. src_具名插槽/components/Category.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="category">
+        <h3>{{type}}分类</h3>
+        <slot name='content'>当插槽没有设置内容时,我将会显示</slot>
+        <slot name="footer">当插槽没有设置内容时,我将会显示</slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Category',
+    props:['type']
+}
+</script>
