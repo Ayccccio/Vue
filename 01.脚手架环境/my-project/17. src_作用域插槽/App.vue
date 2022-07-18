d8cca1b07ded08bdd202e330a160e402cb0f7902
Ayccccio_home <Ayccccio@gmail.com>
2022-07-18 22:41:09 +0800

    作用域插槽

---
+++ 01.脚手架环境/my-project/17. src_作用域插槽/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="app">
+        <Category type="游戏">
+            <!-- 完整写法 v-slot:content={games} -->
+            <template #content={games}>
+                <ul>
+                    <li v-for="(game,index) in games" :key="index">{{game}}</li>
+                </ul>
+            </template>
+            <div slot="footer">
+                <a href="#">游戏传送门</a>
+                <a href="#">更多游戏</a>
+            </div>
+        </Category>
+    </div>
+</template>
+
+<script>
+import Category from "./components/Category.vue";
+export default {
+    name: "App",
+    components: { Category },
+};
+</script>
+
+<style lang='less'>
+#app {
+    display: flex;
+    justify-content: space-evenly;
+    .category {
+        width: 200px;
+        height: 300px;
+        background-color: #85a4ed;
+        h3 {
+            background-color: orange;
+            text-align: center;
+            font-weight: bold;
+        }
+        img,
+        video {
+            width: 100%;
+        }
+        div {
+            display: flex;
+            justify-content: space-evenly;
+        }
+    }
+}
+</style>
---
+++ 01.脚手架环境/my-project/17. src_作用域插槽/components/Category.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="category">
+        <h3>{{type}}分类</h3>
+        <slot name='content' :games="games">当插槽没有设置内容时,我将会显示</slot>
+        <slot name="footer" >当插槽没有设置内容时,我将会显示</slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Category',
+    props:['type'],
+    data() {
+        return {
+            foods: ["火锅", "烧烤", "小龙虾", "牛排"],
+            games: ["红色警戒", "穿越火线", "劲舞团", "超级玛丽"],
+            films: [
+                "《教父》",
+                "《拆弹专家》",
+                "《你好，李焕英》",
+                "《尚硅谷》",
+            ],
+        };
+    },
+}
+</script>
