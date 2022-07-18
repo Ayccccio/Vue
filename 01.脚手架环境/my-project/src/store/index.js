891cfb65a3de42547a888d202bc1c8d2b65c99ac
Ayccccio_home <Ayccccio@gmail.com>
2022-07-18 22:53:48 +0800

    vux

---
+++ 01.脚手架环境/my-project/src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import count from './count'
+import person from './person'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    modules:{
+        count,
+        person
+    }
+})
